173da73d803c50d50815cf378e7d1866cb4425f0
Frank Gonzalez Lopez <90103594+Banshe666@users.noreply.github.com>
2025-01-19 01:59:57 -0500

    Update Module_2_Lab_Case_Study_Frank_Gonzalez_Lopez.py

--- Module_2/Module_2_Lab_Case_Study_Frank_Gonzalez_Lopez.py
+++ Module_2/Module_2_Lab_Case_Study_Frank_Gonzalez_Lopez.py
@@ -16,35 +16,34 @@ Source Code:
 
 
 def student_checker():
-    print("Student Qualification Checker.\n")
-
+    print("Welcome to the Student Qualification Checker.\n")
+    
     while True:
-        
-        last_name = input("Enter the student's last name (or 'ZZZ' to quit): ").strip()  # Ask for the student's last name
+        last_name = input("Enter the student's last name Or type zzz to quit: ").strip()  # Ask for the student's last name
 
-        
-        if last_name.upper() == 'ZZZ': # Quit if the last name is 'ZZZ'
-            print("Exiting the program")
+        if last_name.upper() == 'ZZZ':  # Quit if the last name is 'ZZZ'
+            print("Exiting the program...")
             break
-
         
-        first_name = input("Enter the student's first name: ").strip() # Ask for the student's first name
-
+        first_name = input("Enter the student's first name: ").strip()  # Ask for the student's first name
         
-        try:  # Ask for the student's GPA
-            student_gpa = float(input("Enter the student's GPA: ").strip())
-        except ValueError:
-            print("Invalid GPA. Please enter a valid number.")
+        gpa = input("Enter the student's GPA: ").strip()  # Ask for the student's GPA
+        
+        if not gpa.replace('.', '', 1).isdigit():  # Check if GPA is a valid number
+            print("GPA must be a valid number. Try again.\n")
             continue
-
         
-        if student_gpa >= 3.5:   # Check qualifications
-            print(f"Congratulations, {first_name} {last_name}! You have made the Dean's List.")
-        elif student_gpa >= 3.25:
-            print(f"Well done, {first_name} {last_name}! You have made the Honor Roll.")
+        gpa = float(gpa)  # Convert GPA to a float number
+        
+        if gpa >= 3.5:
+            print(f"Congratulations, {first_name} {last_name}! You made the Dean's List")
+        elif gpa >= 3.25:
+            print(f"Well done, {first_name} {last_name}! You made the Honor Roll")
         else:
-            print(f"Keep working , {first_name} {last_name}. You are not on the Dean's List or the Honor Roll.")
+            print(f"Don't give up, {first_name} {last_name}.")
+        
+        
 
+student_checker()  # Run the main function
 
-student_checker() # Run the main function
 
