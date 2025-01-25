021b306057a280c2b1f5fce1ad41094c7d0549e0
Frank Gonzalez Lopez <90103594+Banshe666@users.noreply.github.com>
2025-01-25 18:14:56 -0500

    Add files via upload

---
+++ Module_3/Module 3 Programming Assignment - Lists and Functions/Module_3_Programming_7_4_to_7_7.py
@@ -0,0 +1,11 @@
+
+things = ['mozzarella', 'cinderella', 'salmonella']
+
+things[1] = things[1].capitalize()
+print(things)
+
+things[0] = things[0].upper()
+print(things)
+
+things.pop(2)
+print(things)
